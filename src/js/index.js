3e558ba607f055d64fc8ba97206be86d76d75945
Juan Felipe Obregon Rodriguez <51882254+JuanFelipeObregonRodriguez@users.noreply.github.com>
2022-04-04 18:08:43 -0500

    rate

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,44 @@
 const numberButtons = document.querySelectorAll(".vote-btn");
 const submitRatingBtn = document.getElementById("submitRatingBtn");
-const ratingCard = document.getElementById("ratingCard");
-const feedbacCard = document.getElementById("feedbackCard");
-const numberRating = document.getElementById("numberRating");
-let selectedRating = null;
+const bodyPrint = document.getElementById("body");
+let selectedRating;
 
-function handleClickNumberBtn({ target }) {
-  removeAllActiveClass();
-  target.classList.add("active");
-  selectedRating = target.value;
-}
-
-function removeAllActiveClass() {
-  numberButtons.forEach((button) => button.classList.remove("active"));
-}
-
-function handleRatingSubmit() {
-  if (!selectedRating) {
-    return alert("Please select number rating!");
+numberButtons.forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    selectedRating = button.innerHTML;
   }
-
-  numberRating.textContent = selectedRating;
-  feedbacCard.classList.remove("hidden");
-  ratingCard.classList.add("hidden");
-}
-
-numberButtons.forEach((button) => {
-  button.addEventListener("click", handleClickNumberBtn);
+  )
 });
-submitRatingBtn.addEventListener("click", handleRatingSubmit);
+
+const sumitted = ()=>{
+
+  const htmlText =
+   `<div id="feedbackCard" id="feedback">
+
+  <img src="./src/assets/illustration-thank-you.svg" alt="">
+  <div class="vote-bage">
+    you selected ${selectedRating} out of 5
+  </div>
+  <div class="text">
+    <h2> Thank you!</h2>
+    <p> We appreciate you taking the time to give a rating. If you ever need more support,
+      don’t hesitate to get in touch! </p>
+  </div>
+
+</div>`
+
+bodyPrint.innerHTML = htmlText;
+};
+
+submitRatingBtn.addEventListener('click', ()=>{
+  if(selectedRating > 0){
+    sumitted()
+  }else{
+    alert('please, choose something!');
+    window.location.reload;
+  }
+})
+
+
+
+
